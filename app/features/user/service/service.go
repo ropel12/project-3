03c506544b2a2579a4e3ab73dbe714752aa679b7
satrio wibowo <wibowosatrio2002@gmail.com>
2023-04-21 04:22:52 +0700

    Add feature register for user

--- app/features/user/service/service.go
+++ app/features/user/service/service.go
@@ -19,6 +19,7 @@ type (
 	}
 	UserService interface {
 		Login(ctx context.Context, req entity.LoginReq) (int, error)
+		Register(ctx context.Context, req entity.RegisterReq) error
 	}
 )
 
@@ -37,7 +38,35 @@ func (u *user) Login(ctx context.Context, req entity.LoginReq) (int, error) {
 	}
 	if err := helper.VerifyPassword(user.Password, req.Password); err != nil {
 		u.dep.Log.Errorf("Error Service : %v", err)
-		return 0, errorr.NewBad("Password Salah")
+		return 0, errorr.NewBad("Wrong password")
 	}
 	return int(user.ID), nil
+}
+
+func (u *user) Register(ctx context.Context, req entity.RegisterReq) error {
+	if err := u.validator.Struct(req); err != nil {
+		u.dep.Log.Errorf("Error service: %v", err)
+		return errorr.NewBad("Request body not valid")
+	}
+	_, err := u.repo.FindByEmail(u.dep.Db.WithContext(ctx), req.Email)
+	if err == nil {
+		return errorr.NewBad("Email already registered")
+	}
+	passhash, err := helper.HashPassword(req.Password)
+	if err != nil {
+		u.dep.Log.Errorf("Erorr service: %v", err)
+		return errorr.NewBad("Register failed")
+	}
+	data := entity.User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Address:  req.Address,
+		Password: passhash,
+		Image:    "default.jpg",
+	}
+	err = u.repo.Create(u.dep.Db.WithContext(ctx), data)
+	if err != nil {
+		return errorr.NewInternal("Failed to create account")
+	}
+	return nil
 }
