4fdf2d045c225ba4b6779da682046706ed4f195a
satrio wibowo <wibowosatrio2002@gmail.com>
2023-04-24 10:38:23 +0700

    add feature cart

--- app/features/regis.go
+++ app/features/regis.go
@@ -3,6 +3,8 @@ package features
 import (
 	eventrepo "github.com/ropel12/project-3/app/features/event/repository"
 	eventserv "github.com/ropel12/project-3/app/features/event/service"
+	trxrepo "github.com/ropel12/project-3/app/features/transaction/repository"
+	trxserv "github.com/ropel12/project-3/app/features/transaction/service"
 	userrepo "github.com/ropel12/project-3/app/features/user/repository"
 	userserv "github.com/ropel12/project-3/app/features/user/service"
 	"go.uber.org/dig"
@@ -15,6 +17,9 @@ func RegisterRepo(C *dig.Container) error {
 	if err := C.Provide(eventrepo.NewEventRepo); err != nil {
 		return err
 	}
+	if err := C.Provide(trxrepo.NewTransactionRepo); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -25,5 +30,9 @@ func RegisterService(C *dig.Container) error {
 	if err := C.Provide(eventserv.NewEventService); err != nil {
 		return err
 	}
+	if err := C.Provide(trxserv.NewTransactionService); err != nil {
+		return err
+	}
+
 	return nil
 }
--- app/features/transaction/handler/controller.go
+++ app/features/transaction/handler/controller.go
@@ -1,1 +1,32 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+	entity "github.com/ropel12/project-3/app/features/transaction"
+	"github.com/ropel12/project-3/app/features/transaction/service"
+	"github.com/ropel12/project-3/config/dependcy"
+	"github.com/ropel12/project-3/helper"
+	"go.uber.org/dig"
+)
+
+type Transaction struct {
+	dig.In
+	Service service.TransactionService
+	Dep     dependcy.Depend
+}
+
+func (u *Transaction) CreateCart(c echo.Context) error {
+	var req entity.ReqCart
+	if err := c.Bind(&req); err != nil {
+		u.Dep.Log.Errorf("Error service: %v", err)
+		return c.JSON(http.StatusBadRequest, CreateWebResponse(http.StatusBadRequest, "Invalid Request Body", nil))
+	}
+	req.UID = helper.GetUid(c.Get("user").(*jwt.Token))
+	if err := u.Service.CreateCart(c.Request().Context(), req); err != nil {
+		return CreateErrorResponse(err, c)
+	}
+	return c.JSON(http.StatusOK, CreateWebResponse(http.StatusOK, "Success Operation", nil))
+}
