bdb64b3af5b564c054001fcab2e97534fe53dcb8
satrio wibowo <wibowosatrio2002@gmail.com>
2023-04-24 10:38:23 +0700

    add feature cart

--- app/features/transaction/handler/response.go
+++ app/features/transaction/handler/response.go
@@ -1,1 +1,31 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/ropel12/project-3/errorr"
+)
+
+type (
+	WebResponse struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		Data    any    `json:"data,omitempty"`
+	}
+)
+
+func CreateWebResponse(code int, message string, data any) any {
+	return WebResponse{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func CreateErrorResponse(err error, c echo.Context) error {
+	if err, ok := err.(errorr.BadRequest); ok {
+		return c.JSON(http.StatusBadRequest, CreateWebResponse(http.StatusBadRequest, err.Error(), nil))
+	}
+	return c.JSON(http.StatusInternalServerError, CreateWebResponse(http.StatusInternalServerError, err.Error(), nil))
+}
--- app/features/transaction/repository/repo.go
+++ app/features/transaction/repository/repo.go
@@ -1,1 +1,38 @@
 package repository
+
+import (
+	entity "github.com/ropel12/project-3/app/entities"
+	"github.com/ropel12/project-3/errorr"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+type (
+	transaction struct {
+		log *logrus.Logger
+	}
+
+	TransactionRepo interface {
+		Create(db *gorm.DB, cart entity.Carts) error
+	}
+)
+
+func NewTransactionRepo(log *logrus.Logger) TransactionRepo {
+	return &transaction{log: log}
+}
+
+func (t *transaction) Create(db *gorm.DB, cart entity.Carts) error {
+	res := db.Where("user_id = ? AND type_id = ? AND deleted_at IS NULL", cart.UserID, cart.TypeID).FirstOrCreate(&cart)
+	if res.Error != nil {
+		t.log.Errorf("error db : %v", res.Error)
+		return errorr.NewInternal("Internal server error")
+	}
+	if res.RowsAffected == 0 {
+		cart.Qty += 1
+		if err := db.Model(&cart).Where("user_id = ? AND type_id = ? AND deleted_at IS NULL", cart.UserID, cart.TypeID).Update("qty", cart.Qty).Error; err != nil {
+			t.log.Errorf("error db : %v", err)
+			return errorr.NewInternal("Internal server error")
+		}
+	}
+	return nil
+}
